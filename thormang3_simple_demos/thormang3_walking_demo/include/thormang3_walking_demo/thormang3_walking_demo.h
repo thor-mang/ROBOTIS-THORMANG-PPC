4cbcb2f6d1641e50e857c548b081ea8082b5e975
Jay-Song <hjsong@robotis.com>
2016-06-10 20:08:48 +0900

    ROS C++ coding style is applied.

--- thormang3_simple_demos/thormang3_walking_demo/include/thormang3_walking_demo/thormang3_walking_demo.h
+++ thormang3_simple_demos/thormang3_walking_demo/include/thormang3_walking_demo/thormang3_walking_demo.h
@@ -1,12 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2016, ROBOTIS CO., LTD.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of ROBOTIS nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
 /*
  * thormang3_walking_demo.h
  *
  *  Created on: 2016. 2. 18.
- *      Author: HJSONG
+ *      Author: Jay Song
  */
 
-#ifndef SRC_ROBOTIS_THORMANG_PPC_THORMANG3_WALKING_DEMO_INCLUDE_THORMANG3_WALKING_DEMO_THORMANG3_WALKING_DEMO_H_
-#define SRC_ROBOTIS_THORMANG_PPC_THORMANG3_WALKING_DEMO_INCLUDE_THORMANG3_WALKING_DEMO_THORMANG3_WALKING_DEMO_H_
+#ifndef THORMANG3_WALKING_DEMO_THORMANG3_WALKING_DEMO_H_
+#define THORMANG3_WALKING_DEMO_THORMANG3_WALKING_DEMO_H_
 
 #include <ros/ros.h>
 #include <ros/package.h>
@@ -25,21 +55,17 @@
 #include "thormang3_walking_module_msgs/RemoveExistingStepData.h"
 
 
-#include "walking_module_common.h"
+void initialize();
+
+void moveToInitPose();
+
+void setCtrlModule();
+
+void balanceOn();
+void balanceOff();
+
+void walkForward();
+void walkBackWard();
 
 
-
-void Initialize();
-
-void MoveToInitPose();
-
-void SetCtrlModule();
-
-void BalanceOn();
-void BalanceOff();
-
-void WalkForward();
-void WalkBackWard();
-
-
-#endif /* SRC_ROBOTIS_THORMANG_PPC_THORMANG3_WALKING_DEMO_INCLUDE_THORMANG3_WALKING_DEMO_THORMANG3_WALKING_DEMO_H_ */
+#endif /* THORMANG3_WALKING_DEMO_THORMANG3_WALKING_DEMO_H_ */
